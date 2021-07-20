e09f22ec19b8f65d98baf10a5f87c64d45c52ca4
LANGAN84 <langan.kg@gmail.com>
2021-07-20 15:36:53 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+class Student < User
+    attr_accessor :knowledge
+    
+    def initialize
+        @knowledge = []
+        # @knowledge << self
+    end
+    
+    def learn(string)
+        @knowledge << string
+    end
 end
